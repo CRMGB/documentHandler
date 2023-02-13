8244c692a1283fda7d1f2b2e45e932f11a75992a
Bisby91 <cristianbisby@gmail.com>
2023-02-13 16:28:18 +0000

    Added the login as the root url for heroku convenience

--- userManagement/urls.py
+++ userManagement/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 #now import the views.py file into this code
 from . import views
 urlpatterns=[
-  path('register', views.register, name="register"),
+  path('', views.register, name="register"),
   path("login", views.login_request, name="login")
 ]
