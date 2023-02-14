508d737cbe9f9e7fb6ce71e92cd6bdecfe1c9953
Bisby91 <cristianbisby@gmail.com>
2023-02-14 18:01:36 +0000

    Changed the names of the models to a more intuitive one, added the CSVFileForm, CSVFileModel, bew url for the csv content, new logivc in the view to display the files and get the relation with the rows done, few migrations and edition of the templates.

--- fileManagement/models.py
+++ fileManagement/models.py
@@ -1,5 +1,5 @@
-import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 #Django model utils TimeStampedModel
 class TimestampedModel(models.Model):
@@ -9,12 +9,35 @@ class TimestampedModel(models.Model):
     class Meta:
         abstract = True
 
-class UploadCSVFileModel(TimestampedModel):
+class CSVFileModel(models.Model):
+    file_name = models.CharField(max_length=200)
+    row_count = models.IntegerField(null=True)
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        help_text="User owning this csv file",
+        null=True
+    )
+
+    def __str__(self):
+        return 'File Name: %s.\n Row count: %s' % (
+            self.file_name, self.row_count
+        )
+
+class CSVRowsModel(TimestampedModel):
+    """Contents of the CSV file"""
     book_title = models.CharField(max_length=150)
     book_author = models.CharField(max_length=200)
     publisher_name = models.CharField(max_length=150)
     date_published = models.DateField()
     book_id = models.CharField(unique=True, max_length=150)
+    file = models.ForeignKey(
+        CSVFileModel, 
+        on_delete=models.CASCADE,
+        help_text="File owning this row content",
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return 'Tittle: %s.\n Author: %s.\n Created: %s.\n Updated: %s' % (
